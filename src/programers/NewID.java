22921d1641b98a8930ec772126b23e35a2d7a0bd
JunYoung <ko5642027@naver.com>
2021-12-07 23:32:22 +0900

    신규아이디 3단계 완료

--- src/programers/NewID.java
+++ src/programers/NewID.java
@@ -8,31 +8,49 @@ public class NewID {
 
     public static StringBuilder solution(String new_id) {
 
-        StringBuilder output = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < new_id.length(); i++) {
             int a = new_id.charAt(i);
 
             if (a >= 65 && a <= 90) {
-                output.append((char) (a | 32));
+                sb.append((char) (a | 32));
             } else {
-                output.append((char) (a));
+                sb.append((char) (a));
             }
         }
-        System.out.println(output);
+        System.out.println(sb);
 
         int k = 0;
-        for (int i = 0; i < output.length(); i++) {
-            int a = output.charAt(i);
 
-            if (!((a >= 97 && a <= 122) || (a >= 48 && a <= 57) || (a == 45) || (a == 95) || (a == 46))) {
-                output.deleteCharAt(i + k);
+        for (int i = 0; i < sb.length(); i++) {
+            int a = sb.charAt(i);
+
+            if (!((97 <= a && a <= 122) || (48 <= a && a <= 57) || (a == 45) || (a == 95) || (a == 46))) {
+                sb.deleteCharAt(i + k);
                 k--;
             }
         }
 
-        System.out.println(output);
+        System.out.println(sb);
 
-        return output;
+        int count = 1;
+
+        while(count > 0){
+            count = 0;
+            for (int i = 0; i < sb.length(); i++) {
+                if (i + 1 == sb.length()) {
+                    break;
+                }
+                if ((sb.charAt(i) == 46) && (sb.charAt(i + 1) == 46)) {
+                    sb.deleteCharAt(i + 1);
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(sb);
+
+        return sb;
     }
 }
