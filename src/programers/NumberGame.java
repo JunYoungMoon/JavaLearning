d402737f01a2c966e93ad60674516e1548997660
JunYoung <ko5642027@naver.com>
2021-12-11 19:14:05 +0900

    replace로 문자열 치환
    이전에 NumID를 통해 배운 replace 활용 다른 사람들은 replaceAll을 썼던데 굳이?
    replaceAll은 Pattern 클래스를 사용해서 더 느림.

---
+++ src/programers/NumberGame.java
@@ -0,0 +1,19 @@
+package programers;
+
+public class NumberGame {
+    public static void main(String[] args) {
+
+        System.out.println(solution("2three45sixseven"));
+    }
+
+    public static int solution(String s) {
+        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < words.length; i++) {
+            s = s.replaceAll(words[i], Integer.toString(i));
+
+        }
+
+        return Integer.parseInt(s);
+    }
+}
