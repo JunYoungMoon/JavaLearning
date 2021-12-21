50e57a7317d1405eda2070905c5c2ed6e2ce59ef
JunYoung <ko5642027@naver.com>
2021-12-21 19:06:45 +0900

    링버퍼 큐 dump 추가 및 제네릭변경

--- src/dataStructure/Gqueue.java
+++ src/dataStructure/Gqueue.java
@@ -1,11 +1,11 @@
 package dataStructure;
 
 /**
- * 링 버퍼 큐
+ * 링 버퍼 큐 (제네릭)
  */
-public class RingBufQueue {
+public class Gqueue<E> {
     private int max;    //큐의 용량
-    private int[] que;  //큐 본체
+    private E[] que;    //큐 본체
     private int front;  //첫번째 요소 커서
     private int rear;   //마지막 요소 커서
     private int num;    //현재 데이터수
@@ -20,17 +20,17 @@ public class RingBufQueue {
         }
     }
 
-    public RingBufQueue(int capacity) {
+    public Gqueue(int capacity) {
         rear = front = num = 0;
         max = capacity;
         try {
-            que = new int[max];
+            que = (E[]) new Object[max];
         } catch (OutOfMemoryError e) {
             max = 0;
         }
     }
 
-    public void enque(int x) throws OverflowIntAryQueueException {
+    public void enque(E x) throws OverflowIntAryQueueException {
         if (num >= max) {
             throw new OverflowIntAryQueueException();
         }
@@ -54,9 +54,20 @@ public class RingBufQueue {
         }
     }
 
+    public void dump() {
+        if (num <= 0)
+            System.out.println("큐가 비었습니다.");
+        else {
+            //(i + front)값이 max와 같아지면 que 인덱스를 0으로 돌림
+            for (int i = 0; i < num; i++)
+                System.out.print(que[(i + front) % max] + " ");
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
-        RingBufQueue iaq = new RingBufQueue(3);
+        Gqueue<Integer> iaq = new Gqueue<>(3);
 
         iaq.enque(1);
         iaq.enque(12);
@@ -65,5 +76,6 @@ public class RingBufQueue {
         iaq.deque();
 
         iaq.enque(777);
+        iaq.dump();
     }
 }
