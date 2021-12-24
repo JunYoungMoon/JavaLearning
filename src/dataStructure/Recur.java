40e19e11ae50d9a39dff728ab639bec8e995455d
JunYoung <ko5642027@naver.com>
2021-12-24 23:36:57 +0900

    비재귀 스택 함수 활용

--- src/dataStructure/Recur.java
+++ src/dataStructure/Recur.java
@@ -3,16 +3,35 @@ package dataStructure;
 import java.util.Scanner;
 
 /**
- * 재귀함수
+ * 재귀방식 | 비재귀 스택 활용
  */
 public class Recur {
 
     static void recur(int n) {
-        if (n > 0) {
-            recur(n - 1);
-            System.out.println(n);
-            recur(n - 2);
+        //비재귀 스택 활용
+        Gstack<Integer> gs = new Gstack<>(n);
+
+        while (true) {
+            if (n > 0) {
+                gs.push(n);
+                n = n - 1;
+                continue;
+            }
+            if (!gs.isEmpty()) {
+                n = gs.pop();
+                System.out.println(n);
+                n = n - 2;
+                continue;
+            }
+            break;
         }
+
+//        재귀방식
+//        if (n > 0) {
+//            recur(n - 1);
+//            System.out.println(n);
+//            recur(n - 2);
+//        }
     }
 
     public static void main(String[] args) {
