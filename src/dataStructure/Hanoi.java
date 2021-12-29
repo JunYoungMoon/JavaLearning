d978c5d8db6bdd374940c827e66f662f586c1448
JunYoung <ko5642027@naver.com>
2021-12-29 23:06:55 +0900

    하노이의 탑 완성

--- src/dataStructure/Hanoi.java
+++ src/dataStructure/Hanoi.java
@@ -3,18 +3,18 @@ package dataStructure;
 import java.util.Scanner;
 
 /**
- * 하노이의 탑 (미완성)
- * 어떤 방법을 적용해야 할지에 대한 고민
+ * 하노이의 탑
  */
 public class Hanoi {
+    static int count;
     //no개의 원반을 x번 기둥에서 y번 기둥으로 옮김
     static void move(int no, int x, int y) {
         if (no > 1) {
             move(no - 1, x, 6 - x - y);
         }
-        System.out.println("원반[" + no + "]를 " + x + "기둥에서 " + y + "기둥으로 옮김");
+        System.out.println("원반[" + no + "]를 " + x + "기둥에서 " + y + "기둥으로 옮김 그리고 횟수 :" +count++);
         if (no > 1) {
-            move(no - 1, x, 6 - x - y);
+            move(no - 1, 6 - x - y, y);
         }
     }
 
@@ -23,7 +23,7 @@ public class Hanoi {
 
         System.out.print("원반 개수 : ");
         int n = stdIn.nextInt();
-        
+
         move(n, 1, 3);  //1번 기둥의 n개의 원반을 3번 기둥으로 옮김
     }
 }
