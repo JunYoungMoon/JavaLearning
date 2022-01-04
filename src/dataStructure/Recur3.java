9e4a4d77575cdbacd2d329577e1e7eea0cacc368
JunYoung <ko5642027@naver.com>
2022-01-04 23:27:35 +0900

    주석 수정

--- src/dataStructure/Recur3.java
+++ src/dataStructure/Recur3.java
@@ -5,13 +5,13 @@ public class Recur3 {
     static void recur3(int n) {
         int[] nstk = new int[100];  //실제 값이 저장되는 공간
         int[] sstk = new int[100];  //nstk 배열의 값을 구분하기 위해 사용
-        int ptr = -1;   //포인터
+        int ptr = -1;   //포인터 -1을 준 이유는 선 포인터 증가로 do-while문에서 현재 포인터를 유지 하기 위함.
         int sw = 0;     //구분값
 
         while (true) {
             if (n > 0) {
                 ptr++;  //포인터 증가
-                //nstk,sstk는 항상 동일 선상에 위치시킨다.
+                //nstk, sstk는 항상 동일하게 포인트를 가르킨다.
                 nstk[ptr] = n;
                 sstk[ptr] = sw;
 
@@ -19,9 +19,9 @@ public class Recur3 {
                     n = n - 1;
                 else if (sw == 1) { //우
                     n = n - 2;
-                    sw = 0;
+                    sw ^= sw;   //xor 비트연산 0 초기화 그냥해봄
                 }
-                continue;
+                continue;   //계속해서 depth를 가지기 위함.
             }
 
             do {
@@ -38,13 +38,9 @@ public class Recur3 {
     }
 
     public static void main(String[] args) {
-//        Scanner stdIn = new Scanner(System.in);
-
-//        System.out.print("정수입력 : ");
-
-//        int x = stdIn.nextInt();
-
-
-        recur3(4);
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("정수입력 : ");
+        int x = stdIn.nextInt();
+        recur3(x);
     }
 }
