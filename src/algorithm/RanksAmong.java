990c7587b8dbc94b71e1ac249b7149fbcdad7b4a
JunYoung <ko5642027@naver.com>
2022-01-31 00:09:27 +0900

    등수구하기

---
+++ src/algorithm/RanksAmong.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class RanksAmong {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RanksAmong ra = new RanksAmong();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : ra.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
