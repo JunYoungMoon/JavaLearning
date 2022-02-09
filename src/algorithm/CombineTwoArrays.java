999f6cd8a8f532805cf5ab4333bf40f8858f91e6
JunYoung <ko5642027@naver.com>
2022-02-09 22:35:27 +0900

    두 배열 합치기

--- src/algorithm/CombineTwoArrays.java
+++ src/algorithm/CombineTwoArrays.java
@@ -4,11 +4,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CombineTwoArrays {
-    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1++]);
+            } else {
+                answer.add(b[p2++]);
+            }
+        }
+        while (p1 < n) {
+            answer.add(a[p1++]);
+        }
+        while (p2 < m) {
+            answer.add(b[p2++]);
+        }
 
         return answer;
     }
+
     public static void main(String[] args) {
         CombineTwoArrays cta = new CombineTwoArrays();
         Scanner kb = new Scanner(System.in);
@@ -22,9 +37,9 @@ public class CombineTwoArrays {
         for (int i = 0; i < m; i++) {
             b[i] = kb.nextInt();
         }
-        for (int x: cta.solution(n,m,a,b)
-             ) {
-            System.out.print(x+" ");
+        for (int x : cta.solution(n, m, a, b)
+        ) {
+            System.out.print(x + " ");
         }
     }
 }
