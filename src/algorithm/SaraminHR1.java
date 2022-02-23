f16a84491ef6c2af1525fd1054e32213b9992752
JunYoung <ko5642027@naver.com>
2022-02-23 22:52:59 +0900

    사람인HR 코테 문제

--- src/algorithm/SaraminHR1.java
+++ src/algorithm/SaraminHR1.java
@@ -27,27 +27,27 @@ public class SaraminHR1 {
         input += "a";
         char[] s = input.toCharArray();
         char[] arr = {'a', 'e', 'i', 'o', 'u'};
-        String str = "";
+        StringBuilder str = new StringBuilder("" + s[0]);
         int cnt = 0;
 
         for (int i = 1; i < s.length - 1; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (s[i] == arr[j]) {
+            for (char c : arr) {
+                if (s[i] == c) {
                     cnt++;
                 }
             }
-            for (int j = 0; j < arr.length; j++) {
-                if (s[i + 1] == arr[j]) {
+            for (char c : arr) {
+                if (s[i + 1] == c) {
                     cnt++;
                 }
             }
             if (cnt != 2) {
-                str += s[i];
+                str.append(s[i]);
             }
             cnt = 0;
         }
 
-        return str;
+        return str.toString();
     }
 }
 
