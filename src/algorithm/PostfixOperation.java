1af6a554c1914a99a8f2d0e3fcdf9672b17816ba
JunYoung <ko5642027@naver.com>
2022-03-13 23:30:36 +0900

    후위식 연산

---
+++ src/algorithm/PostfixOperation.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PostfixOperation {
+    public int solution(String str) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PostfixOperation po = new PostfixOperation();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(po.solution(str));
+    }
+}
