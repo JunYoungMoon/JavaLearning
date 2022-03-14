93eae8beff18d2fbbaaadb667495e11689e89aaa
JunYoung <ko5642027@naver.com>
2022-03-14 23:41:45 +0900

    후위식 연산

--- src/algorithm/PostfixOperation.java
+++ src/algorithm/PostfixOperation.java
@@ -8,6 +8,30 @@ public class PostfixOperation {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
 
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) {
+                stack.push(x - 48);
+            } else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                switch (x) {
+                    case '+':
+                        stack.push(lt + rt);
+                        break;
+                    case '-':
+                        stack.push(lt - rt);
+                        break;
+                    case '*':
+                        stack.push(lt * rt);
+                        break;
+                    case '/':
+                        stack.push(lt / rt);
+                        break;
+                }
+            }
+        }
+        answer = stack.get(0);
+
         return answer;
     }
 
