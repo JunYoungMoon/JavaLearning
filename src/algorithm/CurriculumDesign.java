464d663c412f1a83f62dfb3da0581c24abbf245f
JunYoungMoon <ko5642027@naver.com>
2022-03-24 22:26:31 +0900

    교육과정 설계

---
+++ src/algorithm/CurriculumDesign.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class CurriculumDesign {
+    public String solution(String need, String plan){
+        String answer = "YES";
+        Queue<Character> Q = new LinkedList<>();
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        CurriculumDesign cd = new CurriculumDesign();
+        Scanner kb = new Scanner(System.in);
+        String a = kb.next();
+        String b = kb.next();
+        System.out.println(cd.solution(a,b));
+    }
+}
