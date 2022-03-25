930b9b7d714aa03f6bf5cdf33ffe0716448ac82b
JunYoungMoon <ko5642027@naver.com>
2022-03-25 23:50:59 +0900

    교육과정 설계

--- src/algorithm/CurriculumDesign.java
+++ src/algorithm/CurriculumDesign.java
@@ -4,18 +4,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class CurriculumDesign {
-    public String solution(String need, String plan){
+class CurriculumDesign {
+    public String solution(String need, String plan) {
         String answer = "YES";
         Queue<Character> Q = new LinkedList<>();
-
+        for (char x : need.toCharArray()) {
+            Q.offer(x);
+        }
+        for (char x : plan.toCharArray()) {
+            if (Q.contains(x)) {//contains 현재 찾고자 하는 요소가 큐에 들어 있는지 알려주는 메서드
+                if (x != Q.poll()) {
+                    return "NO";
+                }
+            }
+        }
+        if (!Q.isEmpty()) {
+            return "NO";
+        }
         return answer;
     }
+
     public static void main(String[] args) {
         CurriculumDesign cd = new CurriculumDesign();
         Scanner kb = new Scanner(System.in);
         String a = kb.next();
         String b = kb.next();
-        System.out.println(cd.solution(a,b));
+        System.out.println(cd.solution(a, b));
     }
 }
