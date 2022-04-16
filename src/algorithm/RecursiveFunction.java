1f2e3db6d49f2f668890e2172383ba6797f74d43
JunYoungMoon <ko5642027@naver.com>
2022-04-16 23:31:23 +0900

    재귀함수(스택프레임)

---
+++ src/algorithm/RecursiveFunction.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class RecursiveFunction {
+    public void DFS(int n) {
+        if (n == 0) {
+            return;
+        } else {
+            DFS(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        RecursiveFunction rf = new RecursiveFunction();
+
+    }
+}
