b2a017943a142ec41eb76961c968fbc9b16f9e9c
JunYoungMoon <ko5642027@naver.com>
2022-04-23 23:56:50 +0900

    피보나치 재귀 (메모이제이션)

--- src/algorithm/RecursiveFunction.java
+++ src/algorithm/RecursiveFunction.java
@@ -1,21 +1,24 @@
 package algorithm;
 
 public class RecursiveFunction {
+    static int[] fibo;
     public int DFS(int n) {
         if (n == 1) {
-            return 1;
+            return fibo[n]=1;
         } else if (n == 2) {
-            return 1;
+            return fibo[n]=1;
         } else {
-            return DFS(n - 2) + DFS(n - 1);
+            return fibo[n]=DFS(n - 2) + DFS(n - 1);
         }
     }
 
     public static void main(String[] args) {
         RecursiveFunction rf = new RecursiveFunction();
         int n = 7;
+        fibo = new int[n+1];
+        rf.DFS(n);
         for (int i = 1; i < n; i++) {
-            System.out.print(rf.DFS(i) + " ");
+            System.out.print(fibo[i] + " ");
         }
     }
 }
