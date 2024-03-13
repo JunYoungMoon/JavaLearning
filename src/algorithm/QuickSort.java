0d578e42e6d7e088413ed715945c83c57c3afcba
dev2 <ko5642027@naver.com>
2024-03-13 22:08:44 +0900

    퀵소트

---
+++ src/algorithm/QuickSort.java
@@ -0,0 +1,53 @@
+package algorithm;
+
+import java.util.Scanner;
+
+class QuickSort {
+    public int[] solution(int n, int[] arr) {
+        quickSort(arr, 0, n - 1);
+        return arr;
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        QuickSort ss = new QuickSort();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : ss.solution(n, arr)
+        ) {
+            System.out.print(x + " ");
+        }
+    }
+}
