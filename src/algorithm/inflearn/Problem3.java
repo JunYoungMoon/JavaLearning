9943e191c7e53024ff43de1851cf11a67954c2c4
dev2 <ko5642027@naver.com>
2024-07-08 09:58:36 +0900

    인프런 3번 문제 - 문장 속 단어중 가장 긴것 찾기

---
+++ src/algorithm/inflearn/Problem3.java
@@ -0,0 +1,42 @@
+package algorithm.inflearn;
+
+import java.util.Scanner;
+
+/**
+ * 3. 문장 속 단어중 가장 긴것 찾기
+ */
+public class Problem3 {
+    static <S extends CharSequence> S solution(S s) {
+        if (s.length() > 100) {
+            throw new IllegalArgumentException("String length exceeds 100 characters. Please enter a shorter string.");
+        }
+
+        int maxLength = 0;
+        S maxWord = null;
+        S[] words = (S[]) s.toString().split(" ");
+
+        for (S word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+                maxWord = word;
+            }
+        }
+
+        return maxWord;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+//        String str = kb.nextLine();
+
+        StringBuilder sb = new StringBuilder(kb.nextLine());
+
+        try {
+            System.out.println(solution(sb));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
