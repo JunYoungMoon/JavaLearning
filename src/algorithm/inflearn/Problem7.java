f2667eb05bf396cc9fc652df1c658f38b6711487
dev2 <ko5642027@naver.com>
2024-07-11 23:31:28 +0900

    인프런 7번 문제 - 회문 문자열

---
+++ src/algorithm/inflearn/Problem7.java
@@ -0,0 +1,20 @@
+package algorithm.inflearn;
+
+/**
+ * 7. 회문 문자열
+ */
+public class Problem7 {
+    public static void main(String[] args) {
+        String s = "gokog";
+
+        s = s.toLowerCase();
+
+        String rs = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(rs)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
