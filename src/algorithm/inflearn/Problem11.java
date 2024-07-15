84ae89a82c9b1fd941246d58535323dda1f7e744
dev2 <ko5642027@naver.com>
2024-07-15 23:54:11 +0900

    인프런 11번 문제 - 문자열 압축

---
+++ src/algorithm/inflearn/Problem11.java
@@ -0,0 +1,22 @@
+package algorithm.inflearn;
+
+import java.util.Arrays;
+
+/**
+ * 11. 문자열 압축
+ */
+public class Problem11 {
+    public static void main(String[] args) {
+        String s = "KKHSSSSSSSE";
+
+        int[] answer = new int[s.length()];
+
+        int cnt = 1;
+
+        for (int i = 0; i < answer.length; i++) {
+
+        }
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
