635108c124bafacb4d788f80b4b0a83314387d92
dev2 <ko5642027@naver.com>
2024-07-16 23:42:02 +0900

    인프런 12번 문제 - 암호

---
+++ src/algorithm/inflearn/Problem12.java
@@ -0,0 +1,32 @@
+package algorithm.inflearn;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * 12. 암호
+ */
+public class Problem12 {
+    private static final int CHUNK_SIZE = 7;
+    private static final char ONE = '#';
+    private static final char ZERO = '*';
+
+    public static void main(String[] args) {
+        int numOfChars = 4;
+        String encodedString = "#****###**#####**#####**##**";
+
+        decodeString(numOfChars, encodedString);
+    }
+
+    private static void decodeString(int numOfChars, String encodedString) {
+        for (int i = 0; i < numOfChars; i++) {
+            String binaryString = encodedString.substring(0, CHUNK_SIZE)
+                    .replace(ONE, '1')
+                    .replace(ZERO, '0');
+
+            char decodedChar = (char) parseInt(binaryString, 2);
+            System.out.print(decodedChar);
+
+            encodedString = encodedString.substring(CHUNK_SIZE);
+        }
+    }
+}
