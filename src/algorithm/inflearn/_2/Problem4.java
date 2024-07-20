0b17096dac744552989d2e7167d104f5ac0bbb83
dev2 <ko5642027@naver.com>
2024-07-21 00:16:44 +0900

    인프런 2-4번 문제 - 피보나치 수열

--- src/algorithm/inflearn/_2/Problem4.java
+++ src/algorithm/inflearn/_2/Problem4.java
@@ -1,23 +1,19 @@
 package algorithm.inflearn._2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 2-4. 피보나치 수열
  */
 public class Problem4 {
     public static void main(String[] args) {
-        int cnt = 5;
+        int a = 1, b = 1, c, cnt = 5;
 
-        List<Integer> list = new ArrayList<>();
+        System.out.printf(a + " " + b + " ");
 
-        list.add(1);
-        list.add(1);
-
-        for (int i = 0; i < cnt; i++) {
-            list.add(list.indexOf(i) + list.indexOf(i + 1));
-            i = i + 2;
+        for (int i = 2; i < cnt; i++) {
+            c = a + b;
+            System.out.printf(c + " ");
+            a = b;
+            b = c;
         }
     }
 }
