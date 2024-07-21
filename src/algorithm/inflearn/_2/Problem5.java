a8a2c82afcb275d510ae8712b734d00b39a413bc
dev2 <ko5642027@naver.com>
2024-07-21 23:58:32 +0900

    인프런 2-5번 문제 - 2-5. 소수(에라토스테네스 체)

---
+++ src/algorithm/inflearn/_2/Problem5.java
@@ -0,0 +1,26 @@
+package algorithm.inflearn._2;
+
+/**
+ * 2-5. 소수(에라토스테네스 체)
+ */
+public class Problem5 {
+    public static void main(String[] args) {
+        int c = 21;
+        int[] ints = new int[c];
+        int answer = 0;
+
+        for (int i = 2; i < ints.length; i++) {
+
+            if(ints[i] ==0){
+                answer++;
+
+                for (int j = i; j < ints.length; j++) {
+                    if(ints[j] % ints[i] == 0){
+                        ints[j] = 1;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
