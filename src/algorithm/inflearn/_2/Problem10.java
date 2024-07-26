6fc795a6292dc91d1f3c65117339f6afd3387960
dev2 <ko5642027@naver.com>
2024-07-27 00:25:10 +0900

    인프런 2-10번 문제 - 봉우리

--- src/algorithm/inflearn/_2/Problem10.java
+++ src/algorithm/inflearn/_2/Problem10.java
@@ -6,29 +6,32 @@ package algorithm.inflearn._2;
 public class Problem10 {
     public static void main(String[] args) {
         int[][] ints = {
-                {0, 0, 0, 0, 0, 0, 0},
-                {0, 5, 3, 7, 2, 3, 0},
-                {0, 3, 7, 1, 6, 1, 0},
-                {0, 7, 2, 5, 3, 4, 0},
-                {0, 4, 3, 6, 4, 1, 0},
-                {0, 8, 7, 3, 5, 2, 0},
-                {0, 0, 0, 0, 0, 0, 0},
+                {5, 3, 7, 2, 3},
+                {3, 7, 1, 6, 1},
+                {7, 2, 5, 3, 4},
+                {4, 3, 6, 4, 1},
+                {8, 7, 3, 5, 2},
         };
 
-        int cnt = 0;
+        int cnt = 0, n = 5;
 
-        for (int i = 1; i < ints.length -1; i++) {
-            for (int j = 1; j < ints.length -1; j++) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
                 for (int k = 0; k < 4; k++) {
-
-                    if (ints[i - 1][j] > ints[i][j] &&
-                            ints[i][j - 1] > ints[i][j] &&
-                            ints[i + 1][j] > ints[i][j] &&
-                            ints[i][j + 1] > ints[i][j]) {
-                        cnt++;
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && ints[nx][ny] >= ints[i][j]) {
+                        flag = false;
+                        break;
                     }
                 }
-
+                if (flag) {
+                    cnt++;
+                }
             }
         }
         System.out.println(cnt);
