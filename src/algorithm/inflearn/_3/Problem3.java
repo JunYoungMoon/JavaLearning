3ce9f3ac875af0e03fbd5acafd558a19ff745d70
moon <ko5642027@naver.com>
2024-08-06 23:47:06 +0900

    인프런 3-3번 문제  - 최대매출

---
+++ src/algorithm/inflearn/_3/Problem3.java
@@ -0,0 +1,15 @@
+package algorithm.inflearn._3;
+
+/**
+ * 3-3. 최대매출
+ */
+public class Problem3 {
+
+    public static void main(String[] args) {
+        int count = 3;
+        int[] ints = {12, 15, 19, 20, 25, 10, 9, 13, 15, 3};
+
+        //O(n^2)을 O(n)으로 하기 위해서는 양쪽 끝의 변수를 알수는 있지만, 가운데 값을 가져오는것이 힘들다.
+        //최대 매출을 알기 위해서는 가운데 값까지 모두 더해야 할것 같은데
+    }
+}
