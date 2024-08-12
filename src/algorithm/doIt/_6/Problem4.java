85826c8e68ddd63675ddb5f5f645b09d5a4958cd
moon <ko5642027@naver.com>
2024-08-12 23:42:52 +0900

    doit  - 삽입정렬 개선

--- src/algorithm/doIt/_6/Problem4.java
+++ src/algorithm/doIt/_6/Problem4.java
@@ -7,16 +7,14 @@ import java.util.Arrays;
  */
 
 public class Problem4 {
-    static void selectionSort(int[] arr) {
+    static void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            int tmp = arr[i], k = i;
-            for (int j = i - 1; j >= 0 && arr[j] > tmp; j--) {
-                arr[j + 1] = arr[j];
-
-                k = j;
+            int tmp = arr[i], j;
+            for (j = i; j > 0 && arr[j - 1] > tmp; j--) {
+                arr[j] = arr[j - 1];
             }
 
-            arr[k] = tmp;
+            arr[j] = tmp;
         }
 
         System.out.println(Arrays.toString(arr));
@@ -24,6 +22,6 @@ public class Problem4 {
 
     public static void main(String[] args) {
         int[] arr = {6, 2, 3, 4, 1, 3, 7, 1, 2};
-        selectionSort(arr);
+        insertionSort(arr);
     }
 }
