315a2f1621be2aa34f7ece37c5f0987de152579e
moon <ko5642027@naver.com>
2024-08-17 23:44:38 +0900

    doit  - 병합정렬 (단순합치기)

---
+++ src/algorithm/doIt/_6/Problem10.java
@@ -0,0 +1,35 @@
+package algorithm.doIt._6;
+
+import java.util.Arrays;
+
+/**
+ * 6-7 병합정렬 (단순합치기)
+ */
+
+public class Problem10 {
+    static void merge(int[] a, int na, int[] b, int nb, int[] c) {
+        int pa = 0, pb = 0, pc = 0;
+
+        while (pa < na && pb < nb) {
+            c[pc++] = (a[pa] <= b[pb]) ? a[pa++] : b[pb++];
+        }
+
+        while (pa < na) {
+            c[pc++] = a[pa++];
+        }
+
+        while (pb < nb) {
+            c[pc++] = b[pb++];
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {8, 7, 6, 5, 4, 3, 2, 1};
+        int[] b = {1, 2, 3, 4, 5, 6, 7};
+        int[] c = new int[a.length + b.length];
+
+        merge(a, a.length, b, b.length, c);
+        System.out.println(Arrays.toString(c));
+    }
+}
