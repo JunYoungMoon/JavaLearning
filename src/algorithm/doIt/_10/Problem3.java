8cddec3be063fc5f06e6af281f69f7cc7ee37745
moon <ko5642027@naver.com>
2024-09-04 22:34:02 +0900

    doit 10-3  - RedBlack 트리

--- src/algorithm/doIt/_10/Problem3.java
+++ src/algorithm/doIt/_10/Problem3.java
@@ -4,5 +4,10 @@ package algorithm.doIt._10;
 class Problem3 {
     public static void main(String[] args) {
         RedBlackTree tree = new RedBlackTree();
+
+        tree.insert(20);
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(50);
     }
 }
--- src/algorithm/doIt/_10/RedBlackTree.java
+++ src/algorithm/doIt/_10/RedBlackTree.java
@@ -23,11 +23,41 @@ public class RedBlackTree {
     }
 
     private Node root;
-    private final Node TNULL; // 리프를 위한 감시노드
+    private final Node TNULL; // 리프노드
 
+    //키가 0이면서 검은색인 루트 노드 생성
     public RedBlackTree() {
         TNULL = new Node(0);
         TNULL.color = BLACK;
         root = TNULL;
+    }
+
+    // 새 노드 삽입
+    public void insert(int key) {
+        Node node = new Node(key); //새로운 노드를 생성하면서 양쪽 리프노드 연결
+        root = insertRecursive(root, node); // 새로운 노드를 추가 그리고 변경된 root 업데이트
+        insertFix(node); // 현재 노드를 파라미터로 전달
+    }
+
+    private Node insertRecursive(Node current, Node node) {
+        // 재귀 베이스 케이스
+        if (current == TNULL) {
+            return node;
+        }
+
+        if (node.key < current.key) {
+            current.left = insertRecursive(current.left, node);
+            current.left.parent = current;
+        } else {
+            current.right = insertRecursive(current.right, node);
+            current.right.parent = current;
+        }
+
+        return current;
+    }
+
+    //RedBlack 트리 특성 복구
+    private void insertFix(Node node) {
+
     }
 }
